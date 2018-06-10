fe3421bbfa9001230124f1dbb4e0101041b4735e
uchiyama-shintaro <uchiyama-shintaro@imjp.co.jp>
2018-06-10 19:34:35 +0900

    springbootで取りあえず動くもの

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# tournament_api
+# 概要
+テニス大会ページ用のAPI実行用プログラム
+# 実行方法
+## dockerの実行環境立ち上げ
+nginxとtomcatを立ち上げてコンテナが立ち上がったこと確認
+```
+docker-compose up -d --build
+docker-compose ps
+```
+## ビルド（テストパスキップ）&パッケージ&デプロイ
+gradleでビルド（テストパスキップ）してwarファイルパッケージングそしてtomcatにデプロイ
+```
+cd tournament
+gradle -Dskip.tests build war && cp build/libs/tournament-0.0.1-SNAPSHOT.war ../docker/tomcat01/deployment/tournament.war
+```
+## ブラウザから確認
+以下URLをブラウザに打ち込んで内容確認  
+※ローカル環境のhostsファイルに「127.0.0.1 tournament.local」を追記
+```
+http://tournament.local/tournament/
+```
--- tournament/src/main/java/club/uctennis/tournament/TournamentApplication.java
+++ tournament/src/main/java/club/uctennis/tournament/TournamentApplication.java
@@ -2,9 +2,17 @@ package club.uctennis.tournament;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TournamentApplication {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello Spring Boot!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TournamentApplication.class, args);
