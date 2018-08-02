3508e8c8abb163e3231754f79f9bbc50fc6f76a1
uchiyama-shintaro <uchiyama-shintaro@imjp.co.jp>
2018-08-02 21:41:48 +0900

    リファクタリング（メソッド化）

--- tournament/src/main/java/club/uctennis/tournament/services/imp/EntryServiceImpl.java
+++ tournament/src/main/java/club/uctennis/tournament/services/imp/EntryServiceImpl.java
@@ -55,9 +55,6 @@ public class EntryServiceImpl implements EntryService {
   @Autowired
   private JavaMailSender javaMailSender;
 
-  // @Autowired
-  // private VelocityEngine velocityEngine;
-
   private String schema;
   private String host;
   private String port;
@@ -111,42 +108,36 @@ public class EntryServiceImpl implements EntryService {
 
     // 仮登録
     //// 仮登録テーブルに登録
-    PreEntries preEntries = new PreEntries();
-    preEntries.setTournamentId(Integer.parseInt(tournamentId));
-    preEntries.setTeamName(teamName);
-    preEntries.setRepresentiveName(representiveName);
-    preEntries.setEmail(email);
-    preEntries.setPhone(phone);
-    LocalDateTime now = LocalDateTime.now();
+    PreEntries preEntries =
+        createPreEntries(tournamentId, teamName, representiveName, email, phone);
     extPreEntriesMapper.insertPreEntry(preEntries);
 
-    //// ワンタイムトークンを生成
+    //// ワンタイムトークンテーブルに登録
     PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    String digest = passwordEncoder.encode(String.valueOf(preEntries.getId()));
-    OnetimeTokens onetimeToken = new OnetimeTokens();
-    onetimeToken.setPreEntryId(preEntries.getId());
-    onetimeToken.setToken(digest);
-    onetimeToken.setLimitedDate(now.plusDays(1));
-    onetimeTokensMapper.insertSelective(onetimeToken);
+    String token = passwordEncoder.encode(String.valueOf(preEntries.getId()));
+    onetimeTokensMapper.insertSelective(
+        createOnetimeToken(preEntries.getId(), token, LocalDateTime.now().plusDays(1)));
 
     // 仮登録完了メール送信
     UriComponentsBuilder builder = UriComponentsBuilder.newInstance();
-    URI location = builder.scheme(schema).host(host).port(port).path("/regist").build().toUri();
+    URI location =
+        builder.scheme(schema).host(host).port(port).path("/regist/" + token).build().toUri();
 
-    MimeMessage msg = javaMailSender.createMimeMessage();
-    MimeMessageHelper helper = new MimeMessageHelper(msg, true);
     VelocityContext context = new VelocityContext();
     context.put("representiveName", representiveName);
     context.put("validationUrl", location.toString());
-    StringWriter writer = new StringWriter();
+
     VelocityEngine velocityEngine = new VelocityEngine();
     velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
     velocityEngine.setProperty("classpath.resource.loader.class",
         ClasspathResourceLoader.class.getName());
     velocityEngine.init();
 
+    StringWriter writer = new StringWriter();
     velocityEngine.mergeTemplate("/templates/ValidationMail.vm", "UTF-8", context, writer);
 
+    MimeMessage msg = javaMailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(msg, true);
     helper.setFrom("test-from@mexample.com");
     helper.setTo("test-to@mexample.com");
     helper.setSubject("大会仮登録完了");
@@ -156,5 +147,43 @@ public class EntryServiceImpl implements EntryService {
     // レスポンスにセット
     preEntryResponse.setPreEntry(modelMapper.map(preEntries, PreEntry.class));
     return preEntryResponse;
+  }
+
+  /**
+   * 仮申込テーブル用のDTO生成.
+   *
+   * @param tournamentId
+   * @param teamName
+   * @param representiveName
+   * @param email
+   * @param phone
+   * @return
+   */
+  private PreEntries createPreEntries(String tournamentId, String teamName, String representiveName,
+      String email, String phone) {
+    PreEntries preEntries = new PreEntries();
+    preEntries.setTournamentId(Integer.parseInt(tournamentId));
+    preEntries.setTeamName(teamName);
+    preEntries.setRepresentiveName(representiveName);
+    preEntries.setEmail(email);
+    preEntries.setPhone(phone);
+    return preEntries;
+  }
+
+  /**
+   * ワンタイムトークンテーブル用のDTO生成.
+   * 
+   * @param preEntryId
+   * @param token
+   * @param limitedDate
+   * @return
+   */
+  private OnetimeTokens createOnetimeToken(Integer preEntryId, String token,
+      LocalDateTime limitedDate) {
+    OnetimeTokens onetimeToken = new OnetimeTokens();
+    onetimeToken.setPreEntryId(preEntryId);
+    onetimeToken.setToken(token);
+    onetimeToken.setLimitedDate(limitedDate);
+    return onetimeToken;
   }
 }
